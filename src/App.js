a990c9f26f75f8b7cd5a1c2a28e9c21a3b2a8618
Foad <FuadHr>
2021-12-19 11:55:07 +0330

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [color, setColor] = useState();
+
+    useEffect(() => {
+        debugger;
+        const pickr = Pickr.create({
+            el: "#color_input",
+            theme: "monolith",
+            components: {
+                preview: true,
+                opacity: true,
+                hue: true,
+                // Input / output Options
+                interaction: {
+                    hex: true,
+                    rgba: true,
+                    hsla: true,
+                    hsva: true,
+                    cmyk: true,
+                    input: true,
+                    clear: true,
+                    save: true
+                }
+            }
+        });
+        window.dd = pickr;
+        pickr.on("change", function (e) {
+            setColor(e.toRGBA());
+        });
+        pickr.on("show", function (e) {
+            pickr.setColor(color);
+        });
+    }, []);
+
+    return (
+        <div>
+            <div id="color_input">
+
+            </div>
+            <input value={color}/>
+        </div>
+    );
 }
 
 export default App;
